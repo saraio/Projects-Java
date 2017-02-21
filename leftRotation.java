ce06e5d57ffd6388ae9751840f7cc222aeab3844
saraio <saraswathi91088@gmail.com>
2017-02-21 10:37:00 -0500

    Update leftRotation.java

--- leftRotation.java
+++ leftRotation.java
@@ -1,1 +1,35 @@
-import java.io.*;import java.util.*;import java.text.*;import java.math.*;import java.util.regex.*;public class Solution {    public static void main(String[] args) {       Scanner sc = new Scanner(System.in);                int n = sc.nextInt();        int d = sc.nextInt();        int array[] = new int[n];                int firstnum=0;        for (int i=0; i<n; i++) {  array[i] = sc.nextInt();  }        for (int j=1; j<=d; j++) {firstnum=array[0];         for( int k=0;k<n;k++)            {                        if(k<(n-1))                {//System.out.print("k: "+k + " array[k]" +array[k] );                array[k]=array[k+1];               // System.out.print(" " +array[k] ); System.out.println(" ");            }            else if (k==n-1)                {                            array[k]=firstnum;   //System.out.println("else " +k +" "+array[k]);                      }        }        }          for (int i=0; i<n; i++) {              System.out.print(array[i]+" ");}    }}
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {    
+  public static void main(String[] args) {       
+    Scanner sc = new Scanner(System.in);                
+    int n = sc.nextInt();        
+    int d = sc.nextInt();        
+    int array[] = new int[n];                
+    int firstnum=0;        
+    for (int i=0; i<n; i++) 
+    {  
+      array[i] = sc.nextInt();  
+    }        
+    for (int j=1; j<=d; j++) 
+    {
+      firstnum=array[0];         
+      for( int k=0;k<n;k++)            
+      {                        
+        if(k<(n-1))                
+        {                
+          array[k]=array[k+1];                           
+        }            
+        else if (k==n-1)                
+        {                            
+          array[k]=firstnum;                         
+        }        
+      }        
+    }          
+    for (int i=0; i<n; i++) 
+    {              System.out.print(array[i]+" ");
+    }    
+  }}
